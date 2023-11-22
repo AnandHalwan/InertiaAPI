1bff17161d4a263706fb8b3a8dbaabfda46206e7
Anand Halwan <ahalwan124@gmail.com>
2023-11-22 12:10:17 -0500

    [Added basic split requests]

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+var cors = require('cors')
+app.use(cors());
+
 app.use(bodyParser.json());
 const port = process.env.PORT || 3000; 
 
@@ -42,7 +45,69 @@ app.post('/auth/signup', async (req, res) => {
     }
   })
 
-  
+  app.get('/split/get', async(req, res) => {
+    try {
+      console.log("Retrieving split")
+      
+      const {userId, splitId} = req.query;
+      console.log("UserId: ", userId)
+      console.log("SplitId: ", splitId)
+      
+      splitRef = dbAdmin.collection("User").doc(userId).collection("Splits");
+
+      const snapshot = await splitRef.get();
+
+      const splits = []
+      snapshot.forEach((doc) => {
+        splits.push(doc.data())
+      });
+      console.log("Splits: ", splits)
+      res.status(200).json({
+        success: true,
+        message: "Successfully retrieved split",
+        splits: splits
+      })
+    } catch (error) {
+      console.log("Error retrieving split: ", error);
+      res.status(500).json({
+        success: false,
+        message: "Error retrieving split"
+      })
+    }
+  })
+
+  app.post('/split/save', async(req, res) => {
+    try {
+      console.log("Saving split")
+      const {userId, splitDocument} = req.body;
+      console.log("UserId: ", userId)
+      console.log("SplitDocument: ", splitDocument.workouts[0].exercises)
+      const splitId = splitDocument["SplitId"]
+      console.log("SplitId: ", splitId)
+
+      const splitRef = dbAdmin.collection("User").doc(userId).collection("Splits").doc(splitId);
+
+      splitRef.set(splitDocument)
+        .then(function() {
+          console.log("Document successfully written!");
+        })
+        .catch(function(error) {
+          console.error("Error writing document: ", error);
+        });
+
+      res.status(200).json({
+        success: true,
+        message: "Successfully saved split"
+      })
+    } catch (error) {
+      console.log("Error saving split: ", error)
+      res.status(500).json({
+        success: false,
+        message: "Unable to save split"
+      })
+    }
+  })
+
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
